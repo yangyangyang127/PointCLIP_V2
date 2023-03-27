e66fa9fa90b1857368f53f39a8ea6fdd40283dd2
zhuxiangyang <42626732+yangyangyang127@users.noreply.github.com>
2023-03-27 11:37:56 +0800

    Update zeroshot_cls.sh

--- zeroshot_cls/zeroshot_cls.sh
+++ zeroshot_cls/zeroshot_cls.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 # Path to dataset
-DATA=data/modelnet40_ply_hdf5_2048
 DATASET=modelnet40
-# DATA=data/scanobjectnn
 # DATASET=scanobjectnn
 
 TRAINER=PointCLIPV2_ZS
 # Trainer configs: rn50, rn101, vit_b32 or vit_b16
 CFG=vit_b16
 
-export CUDA_VISIBLE_DEVICES=8
+export CUDA_VISIBLE_DEVICES=0
 python main.py \
---root ${DATA} \
 --trainer ${TRAINER} \
 --dataset-config-file configs/datasets/${DATASET}.yaml \
 --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
